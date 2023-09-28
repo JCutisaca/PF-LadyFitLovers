27919676df3b90c91c7fbe5d1ceec45aee92f808
LeonardoRodriguez22 <112113959+LeonardoRodriguez22@users.noreply.github.com>
2023-09-28 16:48:46 -0300

    Update getProduct.js

--- src/controllers/ProductController/getProduct.js
+++ src/controllers/ProductController/getProduct.js
@@ -2,7 +2,6 @@ const { Product } = require('../../db')
 
 const getProduct = async() => {
     const allProduct = await Product.findAll({attributes: { exclude: ['onSale'] },})
-    console.log("esto", allProduct);
     return allProduct
 }
 
