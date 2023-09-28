039526592dc585ea5714971ff8a299c503d0814a
MayDenhoff <mayradenhoff@gmail.com>
2023-09-28 16:41:42 -0300

    getproduct

--- src/handlers/productHandler.js
+++ src/handlers/productHandler.js
@@ -14,6 +14,7 @@ const postProductHandler = async(req, res) => {
 
 const getProductHandler = async(req, res) => {
     try {
+        
         const allProduct = await getProduct()
         res.status(201).json(allProduct)
     } catch (error) {
