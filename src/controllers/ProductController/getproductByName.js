df793ff565229c12533eb02e445ac921c6005640
Jhonathan <3jhoncuco@gmail.com>
2023-09-30 02:33:13 -0300

    Add model category to product find name

--- src/controllers/ProductController/getproductByName.js
+++ src/controllers/ProductController/getproductByName.js
@@ -1,15 +1,16 @@
 const { Op } = require('sequelize')
-const { Product } = require('../../db')
+const { Product, Category } = require('../../db')
 
 const getProductByName = async({name}) => {
- 
-
+   if(!name) throw Error('No se recibio ningun parametro')
     const resultados = await Product.findAll({
         where: {
           name: {
-            [Op.like]: `%${name}%`, // Utiliza Op.like para buscar nombres que contengan la cadena proporcionada
+            [Op.like]: `${name}%`,
           }
-        }
+        }, include: [Category], attributes: {
+        exclude: ['CategoryId']
+      }
       })
       return resultados
 }
