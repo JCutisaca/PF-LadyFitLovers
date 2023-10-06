8b1f4419544c03149ed9ce372dfdbbc35d6d372e
soldier_lucas <3jhoncuco@gmail.com>
2023-10-06 18:36:50 -0300

    add association user/product and post/remove fav

--- src/db.js
+++ src/db.js
@@ -5,18 +5,18 @@ const fs = require('fs');
 const path = require('path');
 const { DB_DEPLOY } = process.env;
 
-// const sequelize = new Sequelize(DB_DEPLOY, {
-//     logging: false,
-//     native: false,
-// });
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/prueba`, {
-  logging: false,
-  native: false,
+const sequelize = new Sequelize(DB_DEPLOY, {
+    logging: false,
+    native: false,
 });
+// const {
+//   DB_USER, DB_PASSWORD, DB_HOST,
+// } = process.env;
+
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/prueba`, {
+//   logging: false,
+//   native: false,
+// });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
