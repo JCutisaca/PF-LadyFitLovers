477f723586032f387ef36893646a6401b20860ca
soldier_lucas <3jhoncuco@gmail.com>
2023-10-06 21:22:44 -0300

    add cart and association with user

--- src/db.js
+++ src/db.js
@@ -5,18 +5,18 @@ const fs = require('fs');
 const path = require('path');
 const { DB_DEPLOY } = process.env;
 
-const sequelize = new Sequelize(DB_DEPLOY, {
-    logging: false,
-    native: false,
-});
-// const {
-//   DB_USER, DB_PASSWORD, DB_HOST,
-// } = process.env;
-
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/prueba`, {
-//   logging: false,
-//   native: false,
+// const sequelize = new Sequelize(DB_DEPLOY, {
+//     logging: false,
+//     native: false,
 // });
+const {
+  DB_USER, DB_PASSWORD, DB_HOST,
+} = process.env;
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/prueba`, {
+  logging: false,
+  native: false,
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -34,13 +34,16 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { User, Product, Category, PurchaseHistory } = sequelize.models;
+const { User, Product, Category, PurchaseHistory, Cart } = sequelize.models;
 
 Product.belongsTo(Category);
 Category.hasMany(Product);
 
 User.hasOne(PurchaseHistory);
 PurchaseHistory.belongsTo(User);
+
+Cart.belongsTo(User)
+User.hasOne(Cart)
 
 User.belongsToMany(Product, { through: 'FavoriteProduct', as: 'FavoriteProducts' });
 Product.belongsToMany(User, { through: 'FavoriteProduct', as: 'FavoritedBy' });
