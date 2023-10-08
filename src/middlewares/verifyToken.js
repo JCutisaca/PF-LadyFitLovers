bc6c5dcc0d2e33d20ef761180b75cca401dff5e6
soldier_lucas <3jhoncuco@gmail.com>
2023-10-08 19:58:13 -0300

    in progress

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,14 +1,19 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
+const { User } = require('../db')
 
 const verifyToken = async (req, res, next) => {
     try {
-        if (!req.headers.authorization) return res.status(500).json({ error: "No tenes autorizacion mi rey" })
+        if (!req.headers.authorization) return res.status(500).json({ error: "Authorization header is required." })
         const token = req.headers.authorization.split(" ")[1]
-        if (!token) return res.status(404).json("Amigo o enemigo????")
+        if (!token) return res.status(404).json("JWT token not found in the Authorization header.")
         const tokenVerified = await jwt.verify(token, JWT_SECRET)
+        // console.log(tokenVerified); // puedo obtener el id del usuario
+        // console.log(req.params);
+        // console.log(!Object.keys(req.body).length);
         req.user = tokenVerified.id
-        next()
+        // const findUser = await User.findOne()
+            next()
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
