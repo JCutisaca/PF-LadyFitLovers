0b646cf878b0ac8b7950d56f7aaf24a4cdc410b6
Matias Gochez <gochezmatias@gmail.com>
2023-10-08 23:34:38 -0300

    Resolucion nodemailer

--- src/helpers/mailUserCreated.js
+++ src/helpers/mailUserCreated.js
@@ -5,46 +5,45 @@ const { google } = require('googleapis')
 const OAuth2 = google.auth.OAuth2
 
 const mailUserCreated = async (email) => {
-    const oAuth2Client = new OAuth2(
-    process.env.NODE_CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    'https://developers.google.com/oauthplayground'
-    )
+  const oAuth2Client = new OAuth2(
+  process.env.NODE_CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  'https://developers.google.com/oauthplayground'
+  )
 
-oAuth2Client.setCredentials({
+  oAuth2Client.setCredentials({
   refresh_token: process.env.REFRESH_TOKEN,
   tls: {
       rejectUnauthorized: false,
-  },
-})
+  },})
 
-const accessToken = await oAuth2Client.getAccessToken()
+  const accessToken = await oAuth2Client.getAccessToken()
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: process.env.EMAIL,
-    clientId: process.env.NODE_CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
-    accessToken: accessToken
-  },
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  tls: { rejectUnauthorized: false},
-});
-;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.EMAIL,
+      clientId: process.env.NODE_CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: accessToken
+    },
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    tls: { rejectUnauthorized: false},}
+  );
 
-const mailObject = {
-    from: process.env.EMAIL,
-    to: email,
-    subject: 'Bienvenido',
-    text: 'Bienvenido a LadyFitLovers' // Cambiado 'body' a 'text'
-};
 
-const result = await transporter.sendMail(mailObject, function(err, data) {
+  const mailObject = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'Bienvenido',
+      text: 'Bienvenido a LadyFitLovers' // Cambiado 'body' a 'text'
+  };
+
+  const result = await transporter.sendMail(mailObject, function(err, data) {
     if (err) {
       console.log("Error " + err);
     } else {
