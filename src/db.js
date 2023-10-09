1241b2f8ca608777d8fb22119dbd0bcf87b86a6b
Matias Gochez <gochezmatias@gmail.com>
2023-10-09 00:34:31 -0300

    Cambio de DB local a Deploy

--- src/db.js
+++ src/db.js
@@ -3,10 +3,9 @@ const { Sequelize } = require("sequelize");
 
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST} = process.env;
+const { DB_DEPLOY } = process.env;
 
-// const sequelize = new Sequelize(DB_DEPLOY, {
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pf`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
     logging: false,
     native: false,
 });
@@ -14,7 +13,7 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
 //   DB_USER, DB_PASSWORD, DB_HOST,
 // } = process.env;
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/prueba`, {
+// const sequelize = new Sequelize(postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/prueba, {
 //   logging: false,
 //   native: false,
 // });
