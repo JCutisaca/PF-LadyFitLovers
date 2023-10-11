d61ed5066040785bac693d5780bf1b48b0b0ab81
soldier_lucas <3jhoncuco@gmail.com>
2023-10-10 21:27:12 -0300

    verify completed

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 const { createOrderHandler, getAllOrdersByUserIdHandler, getAllOrdersHandler, updateOrderByIdHandler, getOrderByIdHandler } = require('../handlers/orderHandler');
+const verifyToken = require('../middlewares/verifyToken');
+const verifyTokenAdmin = require('../middlewares/verifyTokenAdmin');
+const verifyTokenParams = require('../middlewares/verifyTokenParams');
 
 const orderRouter = Router();
 
-orderRouter.post("/create", createOrderHandler)
-orderRouter.get("/allOrders", getAllOrdersHandler) // ruta solo admin
-orderRouter.put("/update", updateOrderByIdHandler)
-orderRouter.get("/id/:id", getOrderByIdHandler)
-orderRouter.get("/user/:userId", getAllOrdersByUserIdHandler)
+orderRouter.post("/create",verifyToken, createOrderHandler)
+orderRouter.get("/allOrders", verifyTokenAdmin, getAllOrdersHandler) // ruta solo admin
+orderRouter.put("/update", verifyTokenAdmin, updateOrderByIdHandler)
+orderRouter.get("/id/:id", verifyTokenAdmin, getOrderByIdHandler)
+orderRouter.get("/user/:userId", verifyTokenParams, getAllOrdersByUserIdHandler)
 
 module.exports = orderRouter;
