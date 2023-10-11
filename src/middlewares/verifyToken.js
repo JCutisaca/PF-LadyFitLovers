827b1c0b7062770f2abc42f4201cb037b11040e6
soldier_lucas <3jhoncuco@gmail.com>
2023-10-11 20:02:44 -0300

    fixed verify userBan

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -6,8 +6,13 @@ const verifyToken = async (req, res, next) => {
     try {
         if (!req.headers.authorization) return res.status(500).json({ error: "Authorization header is required." })
         const token = req.headers.authorization.split(" ")[1]
+        
         if (!token) return res.status(404).json("JWT token not found in the Authorization header.")
         const tokenVerified = await jwt.verify(token, JWT_SECRET)
+        const findUser = await User.findOne({ where: { id: tokenVerified.id } })
+        if(findUser.typeUser === "Admin") {
+            return next()
+        }
         if (req.body && Object.keys(req.body)) {
             if (tokenVerified.id !== req.body.userId) throw Error('User ID in the JWT token does not match the requested user ID.')
         }
