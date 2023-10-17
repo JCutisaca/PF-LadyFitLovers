ba04bbae941cf34a58f335881e05804a2a7f3eb3
Matias Gochez <gochezmatias@gmail.com>
2023-10-17 20:50:29 -0300

    solucion db.js

--- src/db.js
+++ src/db.js
@@ -7,20 +7,11 @@ const { DB_USER, DB_PASSWORD, DB_HOST} = process.env;
 const { DB_DEPLOY } = process.env;
 
 
-// const sequelize = new Sequelize(DB_DEPLOY, {
-//   //const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pf`, {
-//       logging: false,
-//       native: false,
-//   });
-
 const sequelize = new Sequelize(DB_DEPLOY, {
-    logging: false,
-    native: false,
+//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pf`, {
+  logging: false,
+  native: false,
 });
-
-// const {
-//   DB_USER, DB_PASSWORD, DB_HOST,
-// } = process.env;
 
 const basename = path.basename(__filename);
 
