f7d9ef5ce9ad6cd297233ee29956adcdb49dc2fe
soldier_lucas <3jhoncuco@gmail.com>
2023-10-17 13:04:37 -0300

    fixed server

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const server = require('./src/server')
 const { conn } = require('./src/db.js');
 const { PORT } = process.env;
 
+const port = PORT || 3001;
+const host = "0.0.0.0";
 
 conn.sync({ force: false }).then(() => {
-    const port = PORT || 3001
-    server.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
-    })
+  server.listen(port, host, () => {
+    console.log(`Server listening on port ${port}`);
+  })
 }).catch(error => console.error(error))
