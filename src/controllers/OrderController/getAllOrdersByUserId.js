d85e901bfda62586fab74f693e5f845b6bd42f06
soldier_lucas <3jhoncuco@gmail.com>
2023-10-18 12:39:43 -0300

    fixed server

--- src/controllers/OrderController/getAllOrdersByUserId.js
+++ src/controllers/OrderController/getAllOrdersByUserId.js
@@ -3,11 +3,7 @@ const { Order, User } = require('../../db')
 const getAllOrdersByUserId = async ({ userId }) => {
     if (!userId) throw Error("Please provide a valid ID.")
     const userOrders = await Order.findAll({
-        where: { UserId: userId },
-        include: {
-            model: User,
-            attributes: ['id', 'name', 'surname', 'email', 'phone', 'address']
-        }
+        where: { UserId: userId }
     })
     return userOrders;
 }
