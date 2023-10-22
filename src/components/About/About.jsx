37b8b2d7ba8412bb33c4957b736ca0ad89ad6752
soldier_lucas <3jhoncuco@gmail.com>
2023-10-22 19:58:28 -0300

    fixed nav and modified about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
 import "./about.css";
 import AboutCard from "./AboutCard";
+import { useSelector } from "react-redux";
+import MenuBurger from "../MenuBurger/MenuBurger";
 
 
 const About = () => {
 
+  const menuBurger = useSelector(state => state.menuBurger)
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -12,56 +15,56 @@ const About = () => {
   const users=[
     {
       name: 'Alejandra Osuna',
-      description: 'Full Stack Developer, aportes al Front End.',
+      description: 'Full Stack Developer.',
       image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697166111/ale_zfawdj.jpg',
       linkedin: 'https://www.linkedin.com/in/alejandra-osuna-258b8a248/',
       github: 'https://github.com/ruthosuna92',
     },
     {
-      name: 'Marina Solange García',
-      description: 'Full Stack Developer, aportes al Front End.',
-      image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697565079/SOL_mfgxr3.jpg',
-      linkedin: 'https://www.linkedin.com/in/marina-solange-garcia/',
-      github: 'https://github.com/msolangeg',
-    },
-    {
-      name: 'Hebe Lia Romeu',
-      description: 'Full Stack Developer, aportes al Front End.',
-      image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697330695/hebe_pcq2he.jpg',
-      linkedin: 'https://www.linkedin.com/in/hebeliaromeu/',
-      github: 'https://github.com/hebelia',
-    },
-    {
       name: 'Davian Pavon Amado',
-      description: 'Full Stack Developer, aportes al Front End.',
+      description: 'Full Stack Developer.',
       image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697217646/davian_ogvkuv.jpg',
       linkedin: 'https://www.linkedin.com/in/davian-pabon-amado-54105627a/',
       github: 'https://github.com/altair201',
     },
     {
+      name: 'Hebe Lia Romeu',
+      description: 'Full Stack Developer.',
+      image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697330695/hebe_pcq2he.jpg',
+      linkedin: 'https://www.linkedin.com/in/hebeliaromeu/',
+      github: 'https://github.com/hebelia',
+    },
+    {
       name: 'Jhonathan Cutisaca',
-      description: 'Full Stack Developer, aportes al Back End.',
+      description: 'Full Stack Developer.',
       image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697599301/23-09-23_hmqerb.jpg',
       linkedin: 'https://www.linkedin.com/in/jhonathan-cutisaca/',
       github: 'https://github.com/JCutisaca',
     },
     {
-      name: 'Matias Gochez',
-      description: 'Full Stack Developer, aportes al Back End.',
-      image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697563108/matias_t9crxh.jpg',
-      linkedin: 'https://www.linkedin.com/in/matias-gochez-1b2921119/',
-      github: 'https://github.com/MGochez',
-    },
-    {
       name: 'Leonardo Rodríguez',
-      description: 'Full Stack Developer, aportes al Back End.',
+      description: 'Full Stack Developer.',
       image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697155062/a59fecd1-6695-4379-995d-3d15f54734a8_hjsnwk.jpg',
       linkedin: 'https://www.linkedin.com/in/leonardo-rodriguez-803560241/',
       github: 'https://github.com/LeonardoRodriguez22',
     },
     {
+      name: 'Marina Solange García',
+      description: 'Full Stack Developer.',
+      image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697565079/SOL_mfgxr3.jpg',
+      linkedin: 'https://www.linkedin.com/in/marina-solange-garcia/',
+      github: 'https://github.com/msolangeg',
+    },
+    {
+      name: 'Matias Gochez',
+      description: 'Full Stack Developer.',
+      image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697563108/matias_t9crxh.jpg',
+      linkedin: 'https://www.linkedin.com/in/matias-gochez-1b2921119/',
+      github: 'https://github.com/MGochez',
+    },
+    {
       name: 'Mayra Denhoff',
-      description: 'Full Stack Developer, aportes al Back End.',
+      description: 'Full Stack Developer.',
       image: 'https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697687279/LadyFitLovers/may_ok1hna.jpg',
       linkedin: 'https://www.linkedin.com/in/mayradenhoff/',
       github: 'https://github.com/Maydenhoff',
@@ -69,6 +72,7 @@ const About = () => {
   ]
   return (
     <div className="aboutContainer">
+      {menuBurger ? <MenuBurger></MenuBurger> : null}
       <div className="barra">
           <h1 className="titleAbout">Grupo de desarrollo web</h1>
           <p className="pAbout">
