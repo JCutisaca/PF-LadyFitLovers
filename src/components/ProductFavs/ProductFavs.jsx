23eb675744f77df48314be4ba30b6de54f9a3844
altair201 <wilsondavianpa1998@outlook.com>
2023-10-21 21:58:02 -0500

    update profile responsive

--- src/components/ProductFavs/ProductFavs.jsx
+++ src/components/ProductFavs/ProductFavs.jsx
@@ -1,8 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import getFavoritesByIdUser from "../../redux/Actions/Favs/getFavoritesByIdUser";
 // import style from "../Products/Products.module.css"
-import Product from "../Product/Product";
-import { Button, Card, Col, Drawer, Image, Row } from "antd";
+import { Button, Card, Col, Image, Row } from "antd";
 import { useEffect } from "react";
 import NoFoundScreen from "../NoFoundScreen/NoFoundScreen";
 import deleteFav from "../../redux/Actions/Favs/deleteFav";
@@ -12,59 +11,58 @@ import style from "../ShoppingClient/ShoppingClient.module.css";
 
 
 const ProductFavs = () => {
-      const dispatch = useDispatch()
-      const user = useSelector((state)=> state.user)
-      const favorites = useSelector((state)=> state.favorites)
-      const accessToken = useSelector((state) => state.accessToken);
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.user)
+  const favorites = useSelector((state) => state.favorites)
+  const accessToken = useSelector((state) => state.accessToken);
 
-  
-      
-      useEffect(()=> {
-        dispatch(getFavoritesByIdUser(user.id))
 
-      },[])
-      // console.log(favorites, "mis favortios");
 
-    return (
-      <div className={style.shoppingClientContainer}>
-      {!favorites.length  ? <NoFoundScreen text={"No se encontraron favoritos"}/>
-      
-      :favorites?.map((fav) => (
-        <div key={fav.id}>
+  useEffect(() => {
+    dispatch(getFavoritesByIdUser(user.id))
+
+  }, [])
+  // console.log(favorites, "mis favortios");
+
+  return (
+    <div className={style.shoppingClientContainer}>
+      {!favorites.length ? <NoFoundScreen text={"No se encontraron favoritos"} />
+
+        : favorites?.map((fav) => (
+          <div key={fav.id}>
             <Card
               key={fav.id}
               bordered={false}
               hoverable={true}
+              className={style.cardFavs}
               style={{
-                width: "80vh",
-                height: 120,
+                width: 250,
+                height: 250,
                 margin: 5,
+                position: "relative",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
               }}
               id={fav.id}
             >
-              <Row justify="center">
-                <Col span={3}>
-  
 
-                  <div><Image alt={fav.name} src={fav.image && fav.image} width={50} /></div>
-                </Col>
-                <Col span={11}>
-                <NavLink className={style.buy} to={`/detail/${fav.id}`}>
-                  <div >{fav.name}</div>
-                </NavLink>
+              <Button style={{ position: "absolute", top: 5, right: 5 }} id={fav.name} shape='circle' size='small' onClick={() => dispatch(deleteFav(fav.id, user.id, accessToken))}>x</Button>
+              <div className={style.detailFav}>
+              <NavLink className={style.buy} to={`/detail/${fav.id}`}>
+                {fav.name}
+              </NavLink>
+                <div><Image alt={fav.name} src={fav.image && fav.image} width={100} /></div>
                 <div>Precio: ${fav.price}</div>
-                </Col>
-                <Col span={3} >
-                  </Col>
-              <Button id={fav.name} shape='circle' size='small' onClick={() => dispatch(deleteFav(fav.id, user.id, accessToken))}>x</Button>
+              </div>
+             
 
-
-              </Row>
             </Card>
-        </div>
-      ))}
-  </div>
-    )
+          </div>
+        ))}
+    </div>
+  )
 }
 
 export default ProductFavs
