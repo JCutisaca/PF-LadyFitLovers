76f5f9405e19cb476527d0579bb8600d196a3588
soldier_lucas <3jhoncuco@gmail.com>
2023-10-23 09:53:50 -0300

    in progress

--- src/components/GoogleAuth/GoogleAuth.jsx
+++ src/components/GoogleAuth/GoogleAuth.jsx
@@ -1,4 +1,4 @@
-import { GoogleLogin } from "react-google-login";
+// import { GoogleLogin } from "react-google-login";
 import { useDispatch, useSelector } from "react-redux";
 import "./GoogleAuth.css";
 import authUser from "../../redux/Actions/User/authUser";
@@ -12,34 +12,34 @@ const clientId =
   "521123783257-d2stfpejph6ok0djqqpm8e396dsg10c5.apps.googleusercontent.com";
 
 const GoogleAuth = ({ onGoogleLoginSuccess }) => {
-  const dispatch = useDispatch();
-  const userId = useSelector((state) => state.user.id)
-  const accessToken2 = useSelector((state) => state.accessToken)
+//   const dispatch = useDispatch();
+//   const userId = useSelector((state) => state.user.id)
+//   const accessToken2 = useSelector((state) => state.accessToken)
 
- useEffect(() => {
-  if (userId && accessToken2) {
-    dispatch(getCart(userId, accessToken2));
-  }
-}, [userId]);
-  const onSuccess = (res) => {
-    let profileObj = res.profileObj;
-    let accessToken = res.accessToken;
-    if (profileObj.familyName == undefined) {
-      profileObj.familyName = "No definido";
-    }
-    //userId, token, profileObj
-    dispatch(authUser(profileObj, accessToken))
-    onGoogleLoginSuccess();
-  };
+//  useEffect(() => {
+//   if (userId && accessToken2) {
+//     dispatch(getCart(userId, accessToken2));
+//   }
+// }, [userId]);
+//   const onSuccess = (res) => {
+//     let profileObj = res.profileObj;
+//     let accessToken = res.accessToken;
+//     if (profileObj.familyName == undefined) {
+//       profileObj.familyName = "No definido";
+//     }
+//     //userId, token, profileObj
+//     dispatch(authUser(profileObj, accessToken))
+//     onGoogleLoginSuccess();
+//   };
 
-  const onFailure = (res) => {
-    console.log("Login Failed: res: ", res);
-  };
+//   const onFailure = (res) => {
+//     console.log("Login Failed: res: ", res);
+//   };
 
   return (
 
       <div id="signInButton" className="googleButton">
-        
+{/*         
         <GoogleLogin
           clientId={clientId}
           buttonText="Iniciar con Google"
@@ -48,7 +48,7 @@ const GoogleAuth = ({ onGoogleLoginSuccess }) => {
           cookiesPolicy={"single_host_origin"}
           isSignedIn={true}
           
-        />
+        /> */}
       </div>
 
   );
