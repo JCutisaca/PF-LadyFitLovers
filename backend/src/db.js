bb70ccaa1221e5bfb2639b1d6ee6921dfa242647
soldier_lucas <3jhoncuco@gmail.com>
2024-02-04 03:25:13 -0300

    deploy vercel

--- backend/src/db.js
+++ backend/src/db.js
@@ -11,6 +11,7 @@ const sequelize = new Sequelize(DB_DEPLOY, {
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/prueba`, {
   logging: false,
   native: false,
+  dialectModule: require('pg')
 });
 
 const basename = path.basename(__filename);
